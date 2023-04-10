c8d6c369356a3749a9ddcbc757d37462dfafbd75
dmukasa <dmukasa@oberlin.edu>
2023-04-10 13:35:05 -0700

    Add files via upload

--- Pose_Gen.sh
+++ Pose_Gen.sh
@@ -19,7 +19,28 @@ check_h_bond_2AM=${cwd}"/h_bonds_2AM.vmd"
 check_h_bond_APB=${cwd}"/h_bonds_APB.vmd"
 check_h_bond_General=${cwd}"/h_bonds_General.vmd"
 check_h_bond_Strict=${cwd}"/h_bonds_Strict.vmd"
-####################################################################################################################################
+###########################################################PERMISSIONS##################################################################
+#Check the permissions on ORCA, and VMD
+cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+VMD_PATH=$cwd/VMD_1.9.4a55-x86_64-Rev11.app
+ORCA_PATH=$cwd/ORCA/Orca421/orca
+ORCA_NDOINT=$cwd/ORCA/Orca421/orca_ndoint
+ORCA_SCF=$cwd/ORCA/Orca421/orca_scf
+ORCA_UTIL=$cwd/ORCA/Orca421/orca_util
+
+#grant access
+chmod +x $VMD_PATH
+chmod +x $ORCA_PATH
+chmod +x $ORCA_NDOINT
+chmod +x $ORCA_SCF
+chmod +x $ORCA_UTIL
+
+xattr -dr com.apple.quarantine $ORCA_PATH
+xattr -dr com.apple.quarantine $ORCA_NDOINT
+xattr -dr com.apple.quarantine $ORCA_SCF
+xattr -dr com.apple.quarantine $ORCA_UTIL
+########################################################################################################################################
 #####Timing######
 #################
 start=`date +%s`
