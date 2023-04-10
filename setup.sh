ddf8678cf4043662508cedc5fc23e4b032b53b61
dmukasa <dmukasa@oberlin.edu>
2023-04-10 13:30:57 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #Check the permissions on ORCA, and VMD
 cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-VMD_PATH=$cwd/VMD_1.9.4a55-x86_64-Rev11
+VMD_PATH=$cwd/VMD_1.9.4a55-x86_64-Rev11.app
 ORCA_PATH=$cwd/ORCA/Orca421/orca
 ORCA_NDOINT=$cwd/ORCA/Orca421/orca_ndoint
 ORCA_SCF=$cwd/ORCA/Orca421/orca_scf
@@ -13,3 +13,8 @@ chmod +x $ORCA_PATH
 chmod +x $ORCA_NDOINT
 chmod +x $ORCA_SCF
 chmod +x $ORCA_UTIL
+
+xattr -dr com.apple.quarantine $ORCA_PATH
+xattr -dr com.apple.quarantine $ORCA_NDOINT
+xattr -dr com.apple.quarantine $ORCA_SCF
+xattr -dr com.apple.quarantine $ORCA_UTIL
